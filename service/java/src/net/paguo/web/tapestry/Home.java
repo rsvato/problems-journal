5f787dae3a39c7f95473d94782fe765ae708dde6
svr <svr@11230f00-c60a-0410-812f-9e7afa6d8178>
2006-10-05 21:03:50 +0000

    Beginning move to Tapestry
    git-svn-id: http://ns2.paguo.net/svn/repos/network-journal/trunk@177 11230f00-c60a-0410-812f-9e7afa6d8178

--- service/java/src/net/paguo/web/tapestry/Home.java
+++ service/java/src/net/paguo/web/tapestry/Home.java
@@ -1,15 +1,19 @@
 package net.paguo.web.tapestry;
 
 import org.apache.tapestry.html.BasePage;
-import org.apache.tapestry.util.ContentType;
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.annotations.InjectPage;
 
 /**
  * User: slava
  * Date: 06.10.2006
  * Time: 0:49:07
- * Version: $Id$
+ * Version: $Id $
  */
-public class Home extends BasePage {
+public abstract class Home extends BasePage {
+    @InjectPage("Result")
+    public abstract IPage getNextPage();
+
     public String getSubjectName(){
         return "Taburetka";
     }
