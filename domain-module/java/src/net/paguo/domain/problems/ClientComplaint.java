3a021ed20e74f4d0c7c4c076b4eb6314d31808db
svr <svr@11230f00-c60a-0410-812f-9e7afa6d8178>
2006-12-03 23:54:21 +0000

    Changes
    git-svn-id: http://ns2.paguo.net/svn/repos/network-journal/trunk@236 11230f00-c60a-0410-812f-9e7afa6d8178

---
+++ domain-module/java/src/net/paguo/domain/problems/ClientComplaint.java
@@ -0,0 +1,55 @@
+package net.paguo.domain.problems;
+
+import net.paguo.domain.clients.ClientItem;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.validator.NotNull;
+
+import javax.persistence.*;
+
+/**
+ * @author Svyatoslav Reyentenko mailto:rsvato@gmail.com
+ * @version $Id$
+ *          created 26.08.2006 0:19:15
+ * @hibernate.joined-subclass table="complaints"
+ * @hibernate.joined-subclass-key column="c_id"
+ * @hibernate.query name="ClientComplaint.findByClient" query="from ClientComplaint where client = ?"
+ * @hibernate.query name="ClientComplaint.findAll" query="from ClientComplaint order by failureTime"
+ * @hibernate.cache usage="read-write"
+ */
+@Entity
+@Table(name="complaints")
+@PrimaryKeyJoinColumn(name="c_id")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class ClientComplaint extends NetworkFailure {
+    private ClientItem client;
+    private NetworkProblem parent;
+
+    /**
+     * @hibernate.many-to-one class="net.paguo.domain.clients.ClientItem" not-null="true"
+     * @return client
+     */
+    @ManyToOne
+    @NotNull
+    public ClientItem getClient() {
+        return client;
+    }
+
+    public void setClient(ClientItem client) {
+        this.client = client;
+    }
+
+    @ManyToOne
+    public NetworkProblem getParent() {
+        return parent;
+    }
+
+    public void setParent(NetworkProblem parent) {
+        this.parent = parent;
+    }
+
+    public String toString(){
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
---
+++ domain-module/java/src/net/paguo/domain/users/LocalUser.java
@@ -0,0 +1,130 @@
+package net.paguo.domain.users;
+
+import net.paguo.domain.common.PersonalData;
+import net.paguo.domain.common.ContactData;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.NotNull;
+
+/**
+ * @version $Id $
+ * @hibernate.class table="local_users"
+ * @hibernate.cache usage="read-write"
+ * @hibernate.query name="LocalUser.findAll" query="from LocalUser order by familyName"
+ * @hibernate.query name="LocalUser.findByPermission" query="from LocalUser where permissionEntry = ?"
+ */
+@Entity
+@Table(name="local_users")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class LocalUser implements Serializable {
+    private Integer id;
+    private PersonalData personalData;
+    private ContactData contactData;
+    private String description;
+    private Set<LocalRole> roles;
+    private UserPermission permissionEntry;
+
+
+    public LocalUser(){
+        this.personalData = new PersonalData();
+        this.contactData = new ContactData();
+        this.permissionEntry = new UserPermission();
+    }
+    /**
+     * @hibernate.id generator-class="increment"
+     * @return id 
+     */
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name="increment",strategy = "increment")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @hibernate.component class="net.paguo.domain.common.PersonalData"
+     * @return  personal data
+     */
+    @Embedded
+    public PersonalData getPersonalData() {
+        return personalData;
+    }
+
+    public void setPersonalData(PersonalData personalData) {
+        this.personalData = personalData;
+    }
+
+    /**
+     * @hibernate.property
+     * @return  description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @hibernate.set table="roles_users" cascade="save-update"
+     * @hibernate.collection-key column="local_user_id"
+     * @hibernate.collection-many-to-many class="net.paguo.domain.users.LocalRole" column="local_role_id"
+     * @return roles set
+     */
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+        targetEntity = LocalRole.class)
+    @JoinTable(name="roles_users",
+        joinColumns = @JoinColumn(name = "local_user_id"),
+        inverseJoinColumns = @JoinColumn(name = "local_role_id"))
+    public Set<LocalRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<LocalRole> roles) {
+        this.roles = roles;
+    }
+
+    /**
+     * @hibernate.many-to-one class="net.paguo.domain.users.UserPermission" cascade="save-update"
+     *  column="prm" not-null="true"
+     * @return permission entry
+     */
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "prm")
+    @NotNull
+    public UserPermission getPermissionEntry() {
+        return permissionEntry;
+    }
+
+    public void setPermissionEntry(UserPermission permissionEntry) {
+        this.permissionEntry = permissionEntry;
+    }
+
+    /**
+     * @hibernate.component class="net.paguo.domain.common.ContactData"
+     * @return contact data
+     */
+    @Embedded
+    public ContactData getContactData() {
+        return contactData;
+    }
+
+    public void setContactData(ContactData contactData) {
+        this.contactData = contactData;
+    }
+
+    public void addRole(LocalRole role){
+        roles.add(role);
+    }
+}
