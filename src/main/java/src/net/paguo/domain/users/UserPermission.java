7daa8031ab177211bd8615e92449860aaacd1897
svr <svr@11230f00-c60a-0410-812f-9e7afa6d8178>
2007-11-08 22:10:21 +0000

    Copy domain module to maven src
    git-svn-id: http://ns2.paguo.net/svn/repos/network-journal/branches/network-journal-wicket@462 11230f00-c60a-0410-812f-9e7afa6d8178

---
+++ src/main/java/src/net/paguo/domain/users/UserPermission.java
@@ -0,0 +1,43 @@
+package net.paguo.domain.users;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @version $Id $
+ * @hibernate.class table="user_perm"
+ * @hibernate.cache usage="read-write"
+ * @hibernate.query name="findAll" query="from UserPermission order by userName"
+ */
+@Embeddable
+public class UserPermission implements Serializable {
+    private String userName;
+    private String digest;
+
+    /**
+     * @hibernate.id generator-class="assigned"
+     * @return
+     */
+    @Column(name="username",unique = true)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @hibernate.property column="password" not-null="true"
+     * @return
+     */
+    @Column(name="password", nullable = false)
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
+}
