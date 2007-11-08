57a88cdb1dfc4ae562d40c14f702b0deb278e5a3
svr <svr@11230f00-c60a-0410-812f-9e7afa6d8178>
2007-11-08 22:28:16 +0000

    Tests migrated
    git-svn-id: http://ns2.paguo.net/svn/repos/network-journal/branches/network-journal-wicket@469 11230f00-c60a-0410-812f-9e7afa6d8178

---
+++ src/test/java/net/paguo/generic/dao/ClientItemDAOTest.java
@@ -0,0 +1,34 @@
+package net.paguo.generic.dao;
+
+import net.paguo.domain.clients.ClientItem;
+import net.paguo.dao.ClientItemDAO;
+
+import java.util.List;
+
+/**
+ * @author Svyatoslav Reyentenko mailto:rsvato@gmail.com
+ * @version $Id$
+ *          created 23.08.2006 0:35:01
+ */
+public class ClientItemDAOTest extends AbstractDAOTest {
+    private static final String FIRST_CLIENT = "Charles Dickens";
+
+    public void testCreate(){
+        ClientItem item = new ClientItem();
+        item.setClientName(FIRST_CLIENT);
+        item.setDeleted(false);
+        Integer id = getDao().create(item);
+        assertNotNull(id);
+    }
+
+    public void testFind(){
+        List clients = getDao().findByName(FIRST_CLIENT);
+        int s = clients.size();
+        assertTrue(s > 0);
+    }
+
+    @SuppressWarnings("unchecked")
+    private ClientItemDAO getDao(){
+        return (net.paguo.dao.ClientItemDAO) ctx.getBean("clientItemDao");
+    }
+}
