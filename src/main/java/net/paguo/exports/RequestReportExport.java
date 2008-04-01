2673a93fc5edaecd555b401f3f8c5ca84566d703
slava <slava@11230f00-c60a-0410-812f-9e7afa6d8178>
2008-04-01 14:17:20 +0000

    Filling of Excel with change requests
    git-svn-id: http://ns2.paguo.net/svn/repos/network-journal/trunk@840 11230f00-c60a-0410-812f-9e7afa6d8178

--- src/main/java/net/paguo/exports/RequestReportExport.java
+++ src/main/java/net/paguo/exports/RequestReportExport.java
@@ -4,11 +4,14 @@ import net.paguo.controller.ChangeStatusRequestController;
 import net.paguo.domain.requests.ChangeStatusRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.poi.hssf.usermodel.*;
 import org.springframework.context.MessageSource;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author Reyentenko
@@ -17,6 +20,24 @@ public class RequestReportExport {
     private static final Log log = LogFactory.getLog(RequestReportExport.class);
     private ChangeStatusRequestController controller;
     private MessageSource messageSource;
+    private List<String> headers;
+    private List<String> properties;
+
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(List<String> headers) {
+        this.headers = headers;
+    }
+
+    public List<String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<String> properties) {
+        this.properties = properties;
+    }
 
     public ChangeStatusRequestController getController() {
         return controller;
@@ -38,30 +59,64 @@ public class RequestReportExport {
         final Collection<ChangeStatusRequest> requests = getController().getByDates(start, end);
         log.debug("Collection size: " + requests.size());
         HSSFWorkbook wb = new HSSFWorkbook();
+        final HSSFCellStyle style = createHeaderStyle(wb);
+
+        final HSSFSheet hssfSheet = wb.createSheet("Main");
+        addHeader(hssfSheet, style);
+        int i = 1;
+        for (ChangeStatusRequest request : requests) {
+            final HSSFRow row = hssfSheet.createRow(i++);
+            short j = 0;
+            for (String property : properties) {
+               HSSFCell cell = row.createCell(j++);
+               setCellProperty(cell, getObjectValue(request, property));
+            }
+        }
+        log.debug("Workbook successfully created");
+        return wb;
+    }
+
+    protected HSSFCellStyle createHeaderStyle(HSSFWorkbook wb) {
         final HSSFFont font = wb.createFont();
         font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         font.setFontName(HSSFFont.FONT_ARIAL);
         final HSSFCellStyle style = wb.createCellStyle();
         style.setFont(font);
         style.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);
+        return style;
+    }
 
-        final HSSFSheet hssfSheet = wb.createSheet("Main");
-        addHeader(hssfSheet, style);
-        int i = 1;
-        for (ChangeStatusRequest request : requests) {
-            final HSSFRow hssfRow = hssfSheet.createRow(i++);
-            final HSSFCell cell = hssfRow.createCell((short) 0);
-            cell.setCellValue(new HSSFRichTextString(request.getEnteredClient()));
+    protected final Object getObjectValue(Object o, String path){
+        Object property = null;
+        try {
+            property = PropertyUtils.getProperty(o, path);
+        } catch (Throwable t) {
+            property = "";
         }
-        log.debug("Workbook successfully created");
-        return wb;
+        return property;
+    }
+
+    protected final void setCellProperty(HSSFCell cell, Object value){
+        if (value == null){
+            cell.setCellValue(new HSSFRichTextString(""));
+        }else{
+            if (value instanceof Date){
+                cell.setCellValue((Date) value);
+            }else{
+                cell.setCellValue(
+                        new HSSFRichTextString(String.valueOf(value)));
+            }
+        }
     }
 
     private void addHeader(HSSFSheet sheet, HSSFCellStyle style) {
         final HSSFRow row = sheet.createRow(0);
-        final HSSFCell cell = row.createCell((short) 0);
-        cell.setCellStyle(style);
-        cell.setCellValue(new HSSFRichTextString(getMessage("excel.client.header")));
+        int i = 0;
+        for (String header : headers) {
+           final HSSFCell cell = row.createCell((short) i++);
+            cell.setCellStyle(style);
+            cell.setCellValue(new HSSFRichTextString(getMessage(header)));
+        }
     }
 
     private String getMessage(final String code) {
